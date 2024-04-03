6e970bd2fbe4c8e04a68cbd8769262a221d168fe
Daniel Lamper <daniel.lamper@hotmail.com>
2024-04-03 17:12:04 +0200

    Performance improvement WIP

--- debezium_core/src/main/java/keboola/cdc/debezium/SyncStats.java
+++ debezium_core/src/main/java/keboola/cdc/debezium/SyncStats.java
@@ -6,12 +6,19 @@ import java.time.ZonedDateTime;
 
 @Data
 public class SyncStats {
+	private ZonedDateTime startTime;
+	private ZonedDateTime endTime;
 	private ZonedDateTime lastRecord;
 	private int recordCount;
+	private long totalRecords;
 
 	public SyncStats() {
 		this.lastRecord = ZonedDateTime.now();
 		this.recordCount = 0;
+		this.totalRecords = 0;
 	}
 
+	public void addRecords(int recordCount) {
+		this.totalRecords += recordCount;
+	}
 }
